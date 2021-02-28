d42cc8c0419f3b560a20afa86a8ea0687e3f4d33
Devon Walsh <devonwalsh@gmail.com>
2021-02-27 19:25:42 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+    for (i = 0; i < name.length; i++) {
+        console.log(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n = n - 1;
+    }
+}
+
+countDown(10);
